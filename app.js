8af2ffa692ae515d678ee73bd4a664b91c9ea3ed
Ruhani-01 <ruhanimaluja20@gmail.com>
2025-04-29 11:26:01 +0530

    todo

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const taskRoutes = require("./routes/taskRoutes");
+
+dotenv.config();
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use("/api/tasks", taskRoutes);
 
-app.get("/",(req,res)=>{
-    res.json("Hello World");
-})
-
-app.listen(7000,()=>{
-    console.log("App is listening to port 7000")
-})
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(5000, () => console.log("Server running on port 5000")))
+  .catch(err => console.error(err));
